288f9097f71d4725f04c557ba9735eff9eb0f134
Andrew Griffiths <andrewg@felinemenace.org>
2011-09-04 01:20:20 +1000

    Add Logger support, and frame fetching

--- ttyrecTV.rb
+++ ttyrecTV.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'eventmachine'
+require 'logger'
 
 $seen = {}
+$logger = Logger.new('crap.log')
+$logger.level = Logger::DEBUG
 
 # Loop over the processes and find active ttyrec processes. Extracts
 # the filename from the process and monitors it. Stops monitoring files
@@ -18,12 +21,16 @@ def scan_processes()
 		# XXX, extra sanity checking required.
 
 		filename = argv[1]
+		active << filename unless active.include?(filename)
+
 		next unless $seen[filename].nil?
 
-		$seen[filename] = true
+		$logger.debug('Got a new file to monitor (#{filename})')
 
-		puts "argv is #{argv.join(' ')}"
-		active << filename
+		EM.next_tick {
+			$seen[filename] = EM.watch_file(filename, FileHandler)
+		}
+
 	}
 
 	# Handle files no longer being written to
@@ -31,13 +38,54 @@ def scan_processes()
 	old = $seen.keys() - active
 
 	old.each { |filename| 
-		puts "#{filename} is no longer being written"
+		$logger.debug('ttyrec stopped on (#{filename})')
+		# XXX $seen[filename].stop_watching()
 		$seen.delete(filename)
 	}
+
+end
+
+module FileHandler
+	# offset = ?
+	def process_frames
+		frames = []
+
+		fp = open(path, "r")
+		fp.seek(@offset)
+
+		while(true) do
+			header = fp.read(12)
+			return if header.nil?
+
+			t1, t2, len = header.unpack("VVV")
+		
+			ts = t1 + (t2 / 1000000)
+			$logger.debug("length is #{len}, timestamp is #{ts}")
+		
+			data = fp.read(len)
+			return if data.nil?
+
+			print data
+
+			frames << [ ts, data ]
+			@offset += 12 + len
+		end
+
+		fp.close()
+	end
+
+	def initialize
+		@offset = 0
+	end
+
+	def file_modified
+		$logger.debug("#{path} modified")
+		process_frames()
+	end
 end
 
 EventMachine::run do
-	EventMachine::add_periodic_timer(1) do
+	EventMachine::add_periodic_timer(2) do
 		# XXX, run in thread :)
 		scan_processes()
 	end
